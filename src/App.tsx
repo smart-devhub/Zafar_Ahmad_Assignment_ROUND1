e56a65276105ad787dc4270b2ae0cda3b6f8331b
zafarGeek <zafar@geekgenix.com>
2023-01-17 00:53:09 +0500

    change useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 import './App.css';
 import Login from './components/Login';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import {Routes,Route } from 'react-router-dom';
 import Home from './pages/Home';
 import {useNavigate} from 'react-router-dom'
 
 function App() {
   const navigate=useNavigate()
-  
- React.useEffect(()=>{
   const email =localStorage.getItem("email")
+ React.useEffect(()=>{
+  
   if(email){
     navigate('/home')
   }else{
     navigate('/')
   }
- },[])
+ },[email])
  
   return (
     
